368893b85363a8ef8d73a20580edb1f28b31fefb
NetLSS <futureprogrammer@naver.com>
2021-06-07 20:22:57 +0900

    Feat: 알람 리시버 구현

---
+++ app/src/main/java/lilcode/aop/p3/c03/alarm/AlarmReceiver.kt
@@ -0,0 +1,53 @@
+package lilcode.aop.p3.c03.alarm
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    companion object {
+        const val NOTIFICATION_CHANNEL_ID = "1000"
+        const val NOTIFICATION_ID = 100
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        // 채널 생성
+        createNotificationChannel(context)
+        // 알림
+        notifyNotification(context)
+
+    }
+
+    private fun createNotificationChannel(context: Context) {
+        // context : 실행하고 있는 앱의 상태나 맥락을 담고 있음
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                "기상 알람",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            NotificationManagerCompat.from(context)
+                .createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private fun notifyNotification(context: Context) {
+        with(NotificationManagerCompat.from(context)) {
+            val build = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setContentTitle("알람")
+                .setContentText("일어날 시간입니다.")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+            notify(NOTIFICATION_ID, build.build())
+        }
+    }
+
+}
