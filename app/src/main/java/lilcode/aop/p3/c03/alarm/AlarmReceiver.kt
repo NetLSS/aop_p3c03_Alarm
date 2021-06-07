fb896c0f2c7cb2222208e70ab2d4491a768b79bc
NetLSS <futureprogrammer@naver.com>
2021-06-07 20:42:29 +0900

    Feat: 필수 설정값인 아이콘 설정

--- app/src/main/java/lilcode/aop/p3/c03/alarm/AlarmReceiver.kt
+++ app/src/main/java/lilcode/aop/p3/c03/alarm/AlarmReceiver.kt
@@ -45,6 +45,7 @@ class AlarmReceiver : BroadcastReceiver() {
                 .setContentTitle("알람")
                 .setContentText("일어날 시간입니다.")
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
 
             notify(NOTIFICATION_ID, build.build())
         }
