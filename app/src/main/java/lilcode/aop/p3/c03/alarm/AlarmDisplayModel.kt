810e9006084cdf7fb5e133238bcadd674f654cc6
NetLSS <futureprogrammer@naver.com>
2021-06-07 15:45:13 +0900

    Feat: 알람 데이터 모델 작성

---
+++ app/src/main/java/lilcode/aop/p3/c03/alarm/AlarmDisplayModel.kt
@@ -0,0 +1,21 @@
+package lilcode.aop.p3.c03.alarm
+
+data class AlarmDisplayModel(
+    val hour: Int, // 0~23
+    val minute: Int,
+    var onOff: Boolean
+) {
+    val timeText: String
+        get() {
+            val h = "%02d".format(if (hour < 12) hour else hour - 12)
+            val m = "%02d".format(minute)
+
+            return "$h:$m"
+        }
+
+
+    val ampmText: String
+        get() {
+            return if (hour < 12) "AM" else "PM"
+        }
+}
