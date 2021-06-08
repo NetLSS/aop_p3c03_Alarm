81376eee254abdd147e6cbbbb5772606db134647
NetLSS <futureprogrammer@naver.com>
2021-06-08 12:43:54 +0900

    Feat: 코드 개선 및 주석 추가

--- app/src/main/java/lilcode/aop/p3/c03/alarm/MainActivity.kt
+++ app/src/main/java/lilcode/aop/p3/c03/alarm/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.TimePicker
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,42 +28,32 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun renderView(model: AlarmDisplayModel) {
-        // 최초 실행 또는 시간 재설정 시 들어옴
-
-        findViewById<TextView>(R.id.ampmTextView).apply{
-            text = model.ampmText
-        }
-        findViewById<TextView>(R.id.timeTextView).apply{
-            text = model.timeText
-        }
-        findViewById<Button>(R.id.onOffButton).apply{
-            text = model.onOffText
-            tag = model
-        }
-    }
 
     private fun fetchDataFromSharedPreferences(): AlarmDisplayModel {
-        val sharedPreferences = getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val sharedPreferences = getSharedPreferences(M_SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
 
-        val timeDBValue = sharedPreferences.getString(ALARM_KEY, "09:30") ?: "09:30"
-        val onOffDBValue = sharedPreferences.getBoolean(ONOFF_KEY, false)
+        // DB 에서 데이터 가져오기
+        val timeDBValue = sharedPreferences.getString(M_ALARM_KEY, "09:30") ?: "09:30"
+        val onOffDBValue = sharedPreferences.getBoolean(M_ONOFF_KEY, false)
+
+        // 시:분 형식으로 가져온 데이터 스플릿
         val alarmData = timeDBValue.split(":")
 
         val alarmModel = AlarmDisplayModel(alarmData[0].toInt(), alarmData[1].toInt(), onOffDBValue)
 
-        // 보정 조정 예외처리
+        // 보정 조정 예외처 (브로드 캐스트 가져오기)
         val pendingIntent = PendingIntent.getBroadcast(
             this,
-            ALARM_REQUEST_CODE,
+            M_ALARM_REQUEST_CODE,
             Intent(this, AlarmReceiver::class.java),
-            PendingIntent.FLAG_NO_CREATE) // 있으면 가져오고 없으면 안만든다. (null)
+            PendingIntent.FLAG_NO_CREATE
+        ) // 있으면 가져오고 없으면 안만든다. (null)
 
-        if ((pendingIntent == null) and alarmModel.onOff){
+        if ((pendingIntent == null) and alarmModel.onOff) {
             //알람은 꺼져있는데, 데이터는 켜져있는 경우
             alarmModel.onOff = false
 
-        } else if((pendingIntent != null) and alarmModel.onOff.not()){
+        } else if ((pendingIntent != null) and alarmModel.onOff.not()) {
             // 알람은 켜져있는데 데이터는 꺼져있는 경우.
             // 알람을 취소함
             pendingIntent.cancel()
@@ -101,18 +90,19 @@ class MainActivity : AppCompatActivity() {
         val onOffButton = findViewById<Button>(R.id.onOffButton)
         onOffButton.setOnClickListener {
             // 저장한 데이터를 확인한다
-            val model = it.tag as? AlarmDisplayModel ?: return@setOnClickListener// 형변환 실패하는 경우에는 null
-            val newModel = saveAlarmModel(model.hour,model.minute, model.onOff.not()) // on off 스위칭
+            val model =
+                it.tag as? AlarmDisplayModel ?: return@setOnClickListener// 형변환 실패하는 경우에는 null
+            val newModel = saveAlarmModel(model.hour, model.minute, model.onOff.not()) // on off 스위칭
             renderView(newModel)
 
             // 온/오프 에 따라 작업을 처리한다
-            if (newModel.onOff){
+            if (newModel.onOff) {
                 // 온 -> 알람을 등록
                 val calender = Calendar.getInstance().apply {
                     set(Calendar.HOUR_OF_DAY, newModel.hour)
                     set(Calendar.MINUTE, newModel.minute)
                     // 지나간 시간의 경우 다음날 알람으로 울리도록
-                    if (before(Calendar.getInstance())){
+                    if (before(Calendar.getInstance())) {
                         add(Calendar.DATE, 1) // 하루 더하기
                     }
                 }
@@ -123,9 +113,10 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this, AlarmReceiver::class.java)
                 val pendingIntent = PendingIntent.getBroadcast(
                     this,
-                    ALARM_REQUEST_CODE,
+                    M_ALARM_REQUEST_CODE,
                     intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT) // 있으면 새로 만든거로 업데이트
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                ) // 있으면 새로 만든거로 업데이트
 
                 alarmManager.setInexactRepeating( // 정시에 반복
                     AlarmManager.RTC_WAKEUP, // RTC_WAKEUP : 실제 시간 기준으로 wakeup , ELAPSED_REALTIME_WAKEUP : 부팅 시간 기준으로 wakeup
@@ -133,7 +124,7 @@ class MainActivity : AppCompatActivity() {
                     AlarmManager.INTERVAL_DAY, // 하루에 한번씩.
                     pendingIntent
                 )
-            } else{
+            } else {
                 // 오프 -> 알람을 제거
                 cancelAlarm()
             }
@@ -142,13 +133,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun cancelAlarm(){
+    private fun renderView(model: AlarmDisplayModel) {
+        // 최초 실행 또는 시간 재설정 시 들어옴
+
+        findViewById<TextView>(R.id.ampmTextView).apply {
+            text = model.ampmText
+        }
+        findViewById<TextView>(R.id.timeTextView).apply {
+            text = model.timeText
+        }
+        findViewById<Button>(R.id.onOffButton).apply {
+            text = model.onOffText
+            tag = model
+        }
+    }
+
+    private fun cancelAlarm() {
         // 기존에 있던 알람을 삭제한다.
         val pendingIntent = PendingIntent.getBroadcast(
             this,
-            ALARM_REQUEST_CODE,
+            M_ALARM_REQUEST_CODE,
             Intent(this, AlarmReceiver::class.java),
-            PendingIntent.FLAG_NO_CREATE) // 있으면 가져오고 없으면 안만든다. (null)
+            PendingIntent.FLAG_NO_CREATE
+        ) // 있으면 가져오고 없으면 안만든다. (null)
 
         pendingIntent?.cancel() // 기존 알람 삭제
     }
@@ -161,12 +168,12 @@ class MainActivity : AppCompatActivity() {
         )
 
         // time 에 대한 db 파일 생성
-        val sharedPreferences = getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val sharedPreferences = getSharedPreferences(M_SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
 
         // edit 모드로 열어서 작업 (값 저장)
         with(sharedPreferences.edit()) {
-            putString(ALARM_KEY, model.makeDataForDB())
-            putBoolean(ONOFF_KEY, model.onOff)
+            putString(M_ALARM_KEY, model.makeDataForDB())
+            putBoolean(M_ONOFF_KEY, model.onOff)
             commit()
         }
 
@@ -174,12 +181,11 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     companion object {
         // static 영역 (상수 지정)
-        private const val SHARED_PREFERENCE_NAME = "time"
-        private const val ALARM_KEY = "alarm"
-        private const val ONOFF_KEY = "onOff"
-        private val ALARM_REQUEST_CODE = 1000
+        private const val M_SHARED_PREFERENCE_NAME = "time"
+        private const val M_ALARM_KEY = "alarm"
+        private const val M_ONOFF_KEY = "onOff"
+        private val M_ALARM_REQUEST_CODE = 1000
     }
 }
